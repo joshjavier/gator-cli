04eb0541784a835c2d370101442736ec16ceb5e4
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-12-24 15:53:49 +0800

    feat: implement login command

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,67 @@
+import process from "node:process";
 import { readConfig, setUser } from "./config";
+
+type CommandHandler = (cmdName: string, ...args: string[]) => void;
+type CommandsRegistry = Record<string, CommandHandler>;
+
+function handlerLogin(cmdName: string, ...args: string[]) {
+  if (args.length === 0) {
+    throw new Error("username is required");
+  }
+  const username = args[0];
+  setUser(username);
+  console.log(`You have logged in as ${username}.`);
+}
+
+function registerCommand(
+  registry: CommandsRegistry,
+  cmdName: string,
+  handler: CommandHandler,
+) {
+  registry[cmdName] = handler;
+}
+
+function runCommand(
+  registry: CommandsRegistry,
+  cmdName: string,
+  ...args: string[]
+) {
+  try {
+    registry[cmdName](cmdName, ...args);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log("CLI error:", err.message);
+      process.exit(1);
+    }
+    throw err;
+  }
+}
 
 function main() {
   try {
-    setUser("Josh");
-    const config = readConfig();
-    console.log(config);
+    const registry: CommandsRegistry = {};
+    registerCommand(registry, "login", handlerLogin);
+
+    const [, , cmdName, ...args] = process.argv;
+
+    if (!cmdName) {
+      console.log("CLI error: not enough arguments were provided.");
+      process.exit(1);
+    }
+
+    runCommand(registry, cmdName, ...args);
   } catch (err) {
     if (err instanceof Error) {
       // case: no config file
       if (err.message.includes("no such file or directory")) {
-        console.log("~/.gatorconfig.json not found.");
-        return;
+        console.log("Config error: ~/.gatorconfig.json not found.");
+        process.exit(1);
       }
 
       // case: missing fields in config file
       if (err.message.includes("missing field(s)")) {
         console.log("Config error:", err.message);
-        return;
+        process.exit(1);
       }
     }
 
